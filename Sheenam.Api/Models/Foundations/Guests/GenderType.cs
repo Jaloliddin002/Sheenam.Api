931fd250b42a30a8832598f8e805e975c87dd537
Jaloliddin7475 <jamoldinovj7475@gmail.com>
2023-01-11 13:11:08 +0500

    DATA: Model & Migrate Guests

---
+++ Sheenam.Api/Models/Foundations/Guests/GenderType.cs
@@ -0,0 +1,15 @@
+﻿//================================================
+//Copyright (c) Coalication of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//================================================
+
+
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public enum GenderType
+    {
+        Male,
+        Female,
+        Others
+    }
+}
---
+++ Sheenam.Api/Models/Foundations/Guests/Guest.cs
@@ -0,0 +1,21 @@
+﻿//================================================
+//Copyright (c) Coalication of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//================================================
+
+
+namespace Sheenam.Api.Models.Foundations.Guests
+{
+    public class Guest
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirht { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public GenderType Gender { get; set; }
+
+    }
+}
